8023703e1e79faea5b4a60f4637c37d3447fc0a6
Balaji-hub620 <maseedubalaji@gmail.com>
2022-08-09 15:39:07 +0530

    first commit

---
+++ Phase4Section1.4/GoogleSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace Phase4Section1._4
+{
+    [TestClass]
+    public class GoogleSearch
+    {
+        [TestMethod]
+        public void FFSearch()
+        {
+            using (var driver = new ChromeDriver())
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.google.com");
+
+            }
+        }
+    }
+}
